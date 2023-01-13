103e211afdd3d471fcfbf86ad939a98e58e8d935
heypious <patelparam201@gmail.com>
2023-01-13 20:13:27 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 import time
-
 
 # Setting up the Screen
 screen = Screen()
@@ -10,8 +11,11 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
+ball = Ball()
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,5 +26,24 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move()
+    time.sleep(ball.move_speed)
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.y_bounce()
+
+    # DETECT collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+    elif ball.xcor() > 380: #right se nikal gaya
+        ball.reset_postion()
+        scoreboard.l_point()
+
+    elif ball.xcor() < -380: #left se nikal gaya
+        ball.reset_postion()
+        scoreboard.r_point()
+
+
 
 screen.exitonclick()
